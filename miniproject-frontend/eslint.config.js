69225d66c2bbc8956cf695a35794cef888e664fe
unknown <may.pranjali.naik@gmail.com>
2024-10-21 23:09:42 +0530

    second issue

--- miniproject-frontend/eslint.config.js
+++ miniproject-frontend/eslint.config.js
@@ -23,6 +23,9 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    loader:{
+      '.js':'.jsx'
+    },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
--- miniproject-frontend/src/App.jsx
+++ miniproject-frontend/src/App.jsx
@@ -5,17 +5,19 @@ import './index.css';
 import { useState } from 'react';
 import RightSide from './components/RightSide';
 import CustomNavbar from './components/CustomNavbar';
+import Dashboard from './dashboard/dashboard/Dashboard'
 
 function App() {
 const [right,setright] = useState('')
   return (
     <div>
-      <CustomNavbar/>
+      {/* <CustomNavbar/>
     <div className='flex gap-20 justify-center pt-5 p-20'>
 
       <Hero setright={setright}/>
       <RightSide rightside={right} />
-    </div>
+    </div> */}
+    <Dashboard/>
     </div>
     
   )
